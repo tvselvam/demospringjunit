16d858712da60cf9b4cc18a17eabe8c71e6ac113
tvselvam <57443618+tvselvam@users.noreply.github.com>
2023-12-20 13:22:02 +0530

    Initial Commit

---
+++ src/main/java/com/example/demospringjunit/DemospringjunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringjunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringjunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demospringjunit/DemospringjunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringjunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
