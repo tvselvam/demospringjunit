c0ccbbff33b61bdc84682cccd5eadd04c2da4454
tvselvam <thiruvengadaselvam.s@gmail.com>
2023-12-24 19:22:40 +0530

    added home controller

---
+++ src/main/java/com/example/demospringjunit/controller/Home.java
@@ -0,0 +1,13 @@
+package com.example.demospringjunit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "Home Controller";
+    }
+}
---
+++ src/test/java/com/example/demospringjunit/controller/HomeTest.java
@@ -0,0 +1,21 @@
+package com.example.demospringjunit.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class HomeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void welcome() {
+    }
+}
